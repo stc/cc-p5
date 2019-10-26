cf79d17a01d59f755f1b4dfd529c0663b01017a8
stc <stc@binaura.net>
2019-10-26 13:22:43 +0200

    imdb heatmap

--- data/004-imdb/sketch.js
+++ data/004-imdb/sketch.js
@@ -1,8 +1,12 @@
-
 var mData;
-var mFont;
+var myFont;
 
-let data = [];
+var num = 20;
+var gridSize = 30;
+
+var minRank;
+var maxRank;
+var ranks = [];
 
 function preload() {
   mData = loadJSON("data/imdb-top1000.json");
@@ -13,7 +17,13 @@ function setup () {
   createCanvas(windowWidth, windowHeight);
   textFont(myFont);
   textSize(26);
-  console.log();
+
+  for(var i=0; i< num * num; i++) {
+    ranks.push( mData[i]["imdb_rating"] );
+  }
+
+  minRank = Math.min.apply(null, ranks);
+  maxRank = Math.max.apply(null, ranks);
 }
 
 function windowResized () {
@@ -22,7 +32,39 @@ function windowResized () {
 
 function draw() {
   background(255);
-  for(let i=0; i<10; i++) {
-    text(mData[i]["title_eng"], 100, 30 + i * 30);
+  colorMode(HSB);
+  noStroke();
+
+  var index = 0;  
+  for(var i=0; i<num; i++) {
+    for(var j = 0; j<num; j++) {
+      
+       var rank = map(mData[index]["imdb_rating"], minRank, maxRank, 60, 0);
+       fill( rank, 255, 255 );
+ 
+       var x = 100 + i * gridSize;
+       var y = 100 + j * gridSize;
+       
+       rect( x, y, gridSize, gridSize );
+       index += 1;
+    }
+   }
+
+   fill(0);
+
+   var gridIndex = 0;
+   for(var i=0; i<num; i++) {
+      for(var j = 0; j<num; j++) {
+        var x = 100 + i * gridSize;
+        var y = 100 + j * gridSize;
+        
+        if ( (mouseX > x && mouseX < x + gridSize) &&
+            (mouseY > y && mouseY < y + gridSize) ) {
+            text( mData[gridIndex]["title_eng"] + " ( " + 
+                  mData[gridIndex]["imdb_rating"] + " )", mouseX + 30, mouseY );
+        }
+        gridIndex++;
+    }
   }
+  text("First 400 movies of IMDB database. \nColors are based on rating. Hover to see movie's title", 100, 40);
 }
